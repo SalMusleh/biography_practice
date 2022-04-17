00703b9e9be24b54baa1bfbb66e5dc7813ded994
SalMusleh <salmusleh87@gmail.com>
2022-04-17 17:14:32 -0500

    solved biography

--- src/Biography.java
+++ src/Biography.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Biography {
     public static void main(String[] args) {
 
@@ -32,6 +35,63 @@ public class Biography {
          */
 
         //YOUR CODE HERE
+        Scanner input = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
 
+        System.out.println("What is your favorite author's first name?");
+        String firstName = input.next();
+
+        System.out.println("What is your favorite author's last name?");
+        String lastName = input.next();
+
+        System.out.println("Where is your favorite author from?");
+        String country = input.next();
+
+        System.out.println("Is your favorite author alive? (Y/N)");
+        String isAlive1 = input.next();
+
+        boolean isAlive = isAlive1.toUpperCase().startsWith("Y");
+
+        int age = 0;
+
+        if (isAlive) {
+            System.out.println("How old is your favorite author?");
+            age = input.nextInt();
+        }
+
+        String bookInput;
+        
+        do {
+            System.out.println("Would you like to enter book info? (Y/N)");
+            bookInput = input.next();
+            input.nextLine();
+
+            if (bookInput.toUpperCase().startsWith("Y")) {
+
+                System.out.println("What is the name of the book?");
+                String name = input.nextLine();
+
+                System.out.println("What is the genre of the book?");
+                String genre = input.nextLine();
+
+                System.out.println("How many pages does the book have?");
+                int totalPage = input.nextInt();
+
+                Book book = new Book(name, genre, totalPage);
+                books.add(book);
+            }
+        } while (bookInput.toUpperCase().startsWith("Y"));
+
+
+        Author author = new Author(firstName, lastName, country, isAlive, age, books);
+
+        System.out.println("Author's information = " + author);
+
+        if(!books.isEmpty()){
+            System.out.println("Author's books are as listed below: ");
+            for (Book book : books){
+                System.out.println(book);
+            }
+        }
     }
 }
